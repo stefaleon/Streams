8c4d24684715cd749ecfc8ecf860ea9aede0b4ff
stefaleon <stefaleon@gmail.com>
2019-12-01 18:38:37 +0200

    setup routes with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import StreamList from "./components/streams/StreamList";
+import StreamCreate from "./components/streams/StreamCreate";
+import StreamDelete from "./components/streams/StreamDelete";
+import StreamEdit from "./components/streams/StreamEdit";
+import StreamShow from "./components/streams/StreamShow";
 
 import "./App.css";
 
 function App() {
-  return <div>Ohai!</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streams App
+      </Link>
+      <div className="right menu">
+        <Link to="/streams/new" className="item">
+          New Stream
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
