af7a7f3b95d10db267801b92c9b47fc951db44ba
unknown <stefaleon@gmail.com>
2019-12-02 12:39:53 +0200

    keep track of signed-in user id in app level state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import { SIGN_IN, SIGN_OUT } from "./types";
 
-export const signIn = () => {
-  return { type: SIGN_IN };
+export const signIn = userId => {
+  return { type: SIGN_IN, payload: userId };
 };
 
 export const signOut = () => {
