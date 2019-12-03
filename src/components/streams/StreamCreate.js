8251e3a9e25fcba6eea005960cc35a220f80b061
stefaleon <stefaleon@gmail.com>
2019-12-03 22:17:35 +0200

    display material ui styled errors on touch

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,16 +2,29 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderInput = ({ input, label, meta }) => {
     console.log(`meta for ${input.name}: `, meta);
+
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
-        <div className="red-text">{meta.error}</div>
+        {this.renderError(meta)}
       </div>
     );
+  };
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error} </div>
+        </div>
+      );
+    }
   }
+
   onSubmit(formValues) {
     console.log("submitted values from the form: ", formValues);
   }
@@ -21,7 +34,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
