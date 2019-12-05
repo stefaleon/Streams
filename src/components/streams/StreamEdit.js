2cdc3578efdbc3d5014135b42431ed36b9c1f23e
stefaleon <stefaleon@gmail.com>
2019-12-05 16:31:54 +0200

    from StreamEdit pass the reduxForm initialValues to the wrapped StreamForm

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     console.log(this.props);
 
     return (
       <div>
-        {this.props.stream && <div>title: {this.props.stream.title}</div>}
+        <h3> Edit Stream</h3>
+        {this.props.stream && (
+          <StreamForm
+            initialValues={{
+              title: this.props.stream.title,
+              description: this.props.stream.description
+            }}
+            onSubmit={this.onSubmit}
+          />
+        )}
       </div>
     );
   }
@@ -25,4 +39,6 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
