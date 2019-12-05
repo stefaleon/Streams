59c75e5a839486c9a2e41371249de12304d3cc30
stefaleon <stefaleon@gmail.com>
2019-12-05 19:46:15 +0200

    make the modal reusable by passing props from the parent component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+import history from "../../history";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className="ui button">Cancel</button>
+      <button className="ui button negative">Delete</button>
+    </div>
+  );
+
   return (
     <div>
       <div>StreamDelete</div>
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Confirm the deletion of this stream"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   );
 };
