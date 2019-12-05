977345874781b217188838d0d9f6324d816233f7
unknown <stefaleon@gmail.com>
2019-12-05 12:52:04 +0200

    use custom history object - in order to do programmatic navigation from within action creators

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+
+import history from "./history";
 
 import Header from "./components/Header";
 import StreamList from "./components/streams/StreamList";
@@ -16,14 +18,14 @@ function App() {
   return (
     <Provider store={store}>
       <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
-        </BrowserRouter>
+        </Router>
       </div>
     </Provider>
   );
