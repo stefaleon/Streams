a12a2db0316f0d976c826a87052ece8659803560
unknown <stefaleon@gmail.com>
2019-12-06 11:36:31 +0200

    create an FLV player in the StreamShow component

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,15 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const currentId = this.props.match.params.id;
+    this.player = flv.createPlayer({
+      type: "flv",
+      // stream name designated by OBS -> configure OBS for this to work out
+      url: `http://localhost:8000/live/${currentId}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
-    console.log(this.props);
+    console.log("from StreamShow, this.props: ", this.props);
 
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -19,6 +50,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} controls={true} style={{ width: "100%" }} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
